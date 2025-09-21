b16f663d4541e0537629a9b755e72eba85ba9eb1
Art <Artawower@mail.ru>
2025-09-21 23:50:36 +0400

    feat: enhance pair tag jumping

--- lisp/husky-navigation.el
+++ lisp/husky-navigation.el
@@ -31,25 +31,92 @@
         (t nil)))
 
 (defun hn--jump-to-matching-tag ()
-  "Jump to the matching opening/closing tag."
+  "Jump between <tag> and </tag> for the tag at point."
   (interactive)
-  (let* ((line-text (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
-         (bacward-p (string-match-p "</" line-text))
-         (forward-p (string-match-p "<" line-text)))
-
-    (cond
-     ((and bacward-p (fboundp 'sgml-skip-tag-backward))
-      (sgml-skip-tag-backward 1))
-     ((and forward-p (fboundp 'sgml-skip-tag-forward))
-      (sgml-skip-tag-forward 1))
-     (t nil))))
+  (let (name closing beg end qin c)
+    (unless (eq (char-after) ?<)
+      (unless (search-backward "<" nil t)
+        (user-error "No tag at point")))
+    (unless (looking-at "<\\/?[[:alpha:]][[:alnum:]:._-]*\\b")
+      (user-error "No tag at point"))
+    (setq closing (looking-at "</"))
+    (re-search-forward "<\\/?\\([[:alpha:]][[:alnum:]:._-]*\\)\\b")
+    (setq name (match-string-no-properties 1))
+    (goto-char (match-beginning 0))
+    (save-excursion
+      (setq beg (point))
+      (goto-char (1+ beg))
+      (while (and (not end) (not (eobp)))
+        (setq c (char-after))
+        (cond
+         ((and qin (eq c ?\\)) (forward-char 1))
+         ((or (eq c ?\") (eq c ?\'))
+          (if (eq qin c) (setq qin nil) (unless qin (setq qin c))))
+         ((and (not qin) (eq c ?>)) (setq end (point))))
+        (forward-char 1)))
+    (if closing
+        (let ((depth 1) (rx (format "</?%s\\b" (regexp-quote name))))
+          (goto-char beg)
+          (while (and (> depth 0)
+                      (re-search-backward rx nil t))
+            (goto-char (match-beginning 0))
+            (setq depth (if (looking-at (format "</%s\\b" (regexp-quote name)))
+                            (1+ depth) (1- depth))))
+          (when (= depth 0) (goto-char (match-beginning 0))))
+      (let ((depth 1) (rx (format "</?%s\\b" (regexp-quote name))))
+        (goto-char end)
+        (while (and (> depth 0)
+                    (re-search-forward rx nil t))
+          (goto-char (match-beginning 0))
+          (setq depth (if (looking-at (format "</%s\\b" (regexp-quote name)))
+                          (1- depth) (1+ depth)))
+          (goto-char (match-end 0)))
+        (when (= depth 0) (goto-char (match-beginning 0)))))))
 
 (defun bh--jump-to-parenthesis ()
   "Go to the matching parenthesis if on parenthesis."
   (interactive "p")
-  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
-        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
-        (t nil)))
+  (cond
+   ((looking-at "\\s(") (forward-list 1) (backward-char 1))
+   ((looking-at "\\s)") (forward-char 1) (backward-list 1))
+   ((or (looking-at "<") (eq (char-after) ?<)
+        (looking-at ">") (eq (char-before) ?>))
+    (with-syntax-table (copy-syntax-table (syntax-table))
+      (modify-syntax-entry ?< "(")
+      (modify-syntax-entry ?> ")")
+      (cond
+       ((or (looking-at "<") (eq (char-after) ?<))
+        (forward-list 1) (backward-char 1))
+       ((or (looking-at ">") (eq (char-before) ?>))
+        (forward-char 1) (backward-list 1)))))
+   (t nil)))
+
+;; (defun bh--jump-to-parenthesis ()
+;;   "Go to the matching parenthesis if on parenthesis."
+;;   (interactive "p")
+;;   (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+;;         ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+;;         (t nil)))
+
+(defun hn--in-tag-p ()
+  "Return non-nil if point is inside a valid HTML/XML tag."
+  (let ((pos (point)) beg end qin c)
+    (save-excursion
+      (cond
+       ((eq (char-after) ?<) (setq beg (point)))
+       ((search-backward "<" nil t) (setq beg (point))))
+      (when (and beg (looking-at "<\\([!?/]\\|[[:alpha:]]\\)"))
+        (goto-char (1+ beg))
+        (while (and (not end) (not (eobp)))
+          (setq c (char-after))
+          (cond
+           ((and (not qin) (eq c ?<)) (setq beg nil))
+           ((and qin (eq c ?\\)) (forward-char 1))
+           ((or (eq c ?\") (eq c ?\'))
+            (if (eq qin c) (setq qin nil) (unless qin (setq qin c))))
+           ((and (not qin) (eq c ?>)) (setq end (point))))
+          (forward-char 1))))
+    (and beg end (<= beg pos) (<= pos end))))
 
 ;;;###autoload
 (defun husky-navigation-bounce-paren ()
@@ -57,14 +124,14 @@
   (interactive)
   (let ((prev-char (char-to-string (preceding-char)))
         (next-char (char-to-string (following-char))))
-    (cond ((member (following-char) '(?\) ?\] ?\} ?\>)) (bh--jump-to-parenthesis))
+    (cond ((hn--in-tag-p) (hn--jump-to-matching-tag))
+          ((member (following-char) '(?\) ?\] ?\} ?\>)) (bh--jump-to-parenthesis))
           ((string-match "[[{(<]" next-char) (bh--jump-to-parenthesis))
           ((string-match "[\]})>]" prev-char) (bh--jump-to-parenthesis))
           ((eq major-mode 'web-mode) (hn--jump-to-matching-tag))
           ((eq major-mode 'html-mode) (hn--jump-to-matching-tag))
           ((eq major-mode 'html-ts-mode) (hn--jump-to-matching-tag))
           ((eq major-mode 'ng2-html-mode) (hn--jump-to-matching-tag))
-          
 
           (t (progn (message "%s" "Not on a paren, brace, or bracket")
                     nil)))))
